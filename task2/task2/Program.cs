286b777a7fd110148a0616e1a27608c6aac9acac
RashadXalil <rashadmkh@code.edu.az>
2021-08-18 10:26:14 +0400

    task1

---
+++ task2/task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ca5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stringi daxil et  : ");
+            string soz = Console.ReadLine();
+            task1(soz);
+        }
+        static string task1(string soz)
+        {
+
+            string reverse = "";
+            int length = soz.Length - 1;
+
+            while (length >= 0)
+            {
+                reverse += soz[length];
+                length--;
+            }
+            Console.WriteLine(reverse);
+            return reverse;
+        }
+    }
+}
